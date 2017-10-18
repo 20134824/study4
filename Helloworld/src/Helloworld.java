a86b8168855086f66440de8a8a5ba7d1ae09da30
20134824 <32252239+20134824@users.noreply.github.com>
2017-10-18 09:15:38 +0900

    Update Helloworld.java

--- Helloworld/src/Helloworld.java
+++ Helloworld/src/Helloworld.java
@@ -4,7 +4,9 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("Hello Hotfix");
+		System.out.println("Hello Hotfix");\
+			
+		System.out.println("Hello PUSHPrint");
 
 	}
 
